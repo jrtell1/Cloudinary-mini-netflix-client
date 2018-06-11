888bdbe724bcb9076ada0d50c47ce2bd247a9d39
JRTell <john-robin@jrtell.se>
2018-06-11 17:08:29 +0200

    Created config file

---
+++ src/config.js
@@ -0,0 +1,15 @@
+export default {
+  cloudinary: {
+    cloudName: 'jrtell',
+    uploadPreset: 'v63gpw1l'
+  },
+
+  api: {
+    url: 'https://wt-3ab4f5008e0b86a1e53e234e9d9ee5aa-0.sandbox.auth0-extend.com/server'
+  },
+
+  fallbackMovie: {
+    title: 'Black Panther',
+    trailer: 'mini-netflix/Marvel_Studios_Black_Panther_-_Official_Trailer_nmrtr7',
+  }
+}
