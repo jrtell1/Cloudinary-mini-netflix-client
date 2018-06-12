652d057e7301a2733d1b8367e04aa3ea3317471b
JRTell <john-robin@jrtell.se>
2018-06-12 20:28:14 +0200

    Fixed so minimized css for video-player is used

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import 'bulma/css/bulma.css'
-import 'cloudinary-video-player/dist/cld-video-player.css'
+import 'cloudinary-video-player/dist/cld-video-player.light.min.css'
 
 require('cloudinary-video-player');
 window.cloudinary = require('cloudinary-core');
