19c49426a3f71de5a0622a16abdf9525d3b0f7a5
JRTell <john-robin@jrtell.se>
2018-06-26 21:25:52 +0200

    Fixed variable assignment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const serveStatic = require('serve-static');
 
-app = express();
+const app = express();
 app.use(serveStatic(__dirname + '/dist'));
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log('server started ' + port));
