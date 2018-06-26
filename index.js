f83981e855e8da1ad32dc7314fbea8716f210495
JRTell <john-robin@jrtell.se>
2018-06-26 21:24:45 +0200

    Fixes

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const path = require('path');
-const serveStatic = require('serve-static');​
+const serveStatic = require('serve-static');
+
 app = express();
-app.use(serveStatic(__dirname + '/dist'));​
+app.use(serveStatic(__dirname + '/dist'));
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log('server started ' + port));
